b5ee2b3536bbe0b26ad41b1524a42fc0695ecbad
Philippe Saint Jacques <psaintj@MacBook-Pro-de-Philippe.local>
2022-07-11 14:57:30 +0200

    fix(Image): fallback with bg 🎨

--- components/card.tsx
+++ components/card.tsx
@@ -1,25 +1,27 @@
 import styled from 'styled-components';
+import {Add} from '../icons/add';
+import Image from 'next/image';
 
-const MovieCard = ({src, title}) => {
-
-    return (
-        <MovieCard.Wrapper>
-            <MovieCard.Image src={src} />
+const MovieCard = ({src, title}) => (
+    <MovieCard.Wrapper>
+        <MovieCard.Image src={src} width="200" height={266} className="bg-zinc-800"/>
             <MovieCard.Info>
                 <h3>{title}</h3>
             </MovieCard.Info>
-        </MovieCard.Wrapper>
-    )
-}
+        <MovieCard.Add/>
+    </MovieCard.Wrapper>
+)
 
 MovieCard.Wrapper = styled.div`
     height: 100%;
     width: 100%;
     display: flex;
     flex-direction: column;
+    position: relative;
+    cursor: pointer;
 `;
 
-MovieCard.Image = styled.img`
+MovieCard.Image = styled(Image)`
     border-radius: 10px;
 `;
 
@@ -32,6 +34,12 @@ MovieCard.Info = styled.div`
         white-space: nowrap;
         overflow: hidden;
     }
+`
+
+MovieCard.Add = styled(Add)`
+    position: absolute;
+    bottom: 40px;
+    right: 10px;
 `
 
 
